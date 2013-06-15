dd701114baf68459ceaeefd96632623b0a6d5ed6
Charles Johnson <me@charlesjohnson.org>
2013-06-15 03:57:24 -0400

    MAJOR OVERHAUL: PHASE1 APP

---
+++ app/assets/js/application.js
@@ -0,0 +1,218 @@
+angular.module('stashapp', ['ui.compat'])
+	.config(
+	[        '$stateProvider', '$routeProvider', '$urlRouterProvider',
+	function ($stateProvider,   $routeProvider,   $urlRouterProvider) {
+
+	// URL ROUTER PROVIDER
+	$urlRouterProvider
+		.when('/c?id', '/contacts/:id')
+		.otherwise('/');
+
+	// ROUTE PROVIDER
+	$routeProvider
+		.when('/user/:id', {
+			redirectTo: '/contacts/:id',
+		})
+		.when('/', {
+			template: 'This is the homepage',
+		});
+
+	// STATE PROVIDER
+	$stateProvider
+
+		/**
+		 * Notifications
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('notifications', {
+			url: '/notifications',
+			templateUrl: 'notifications.html',
+		})
+
+		/**
+		 * Contacts Section
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('contacts', {
+			url: '/contacts',
+			abstract: true,
+			templateUrl: 'contacts.html',
+			controller:
+				[        '$scope','$state','$http',
+				function ($scope,  $state,  $http) {
+
+					$http.get('/api/friends').success(function(response) {
+						console.log(response);
+						$scope.contacts = response.data;
+					});
+			}],
+		})
+        .state('contacts.list', {
+          // parent: 'contacts',
+          url: '',
+          templateUrl: 'contacts.list.html',
+        })
+
+        // Contacts Detail
+        .state('contacts.detail', {
+          // parent: 'contacts',
+          url: '/{contactId}',
+          resolve: {
+            something:
+              [        '$timeout', '$stateParams',
+              function ($timeout,   $stateParams) {
+                return $timeout(function () { return "Asynchronously resolved data (" + $stateParams.contactId + ")" }, 10);
+              }],
+          },
+          views: {
+            '': {
+              templateUrl: 'contacts.detail.html',
+              controller:
+                [        '$scope', '$stateParams', 'something',
+                function ($scope,   $stateParams,   something) {
+                  $scope.something = something;
+                  $scope.contact = findById($scope.contacts, $stateParams.contactId);
+                }],
+            },
+            'nav-header-left@': {
+              template: '<a href="./#/contacts" class="icons-back"></a>',
+            },
+            'menu': {
+              templateProvider:
+                [ '$stateParams',
+                function ($stateParams){
+                  // This is just to demonstrate that $stateParams injection works for templateProvider
+                  // $stateParams are the parameters for the new state we're transitioning to, even
+                  // though the global '$stateParams' has not been updated yet.
+                  return '<hr><small class="muted">Contact ID: ' + $stateParams.contactId + '</small>';
+                }],
+            },
+          },
+        })
+
+		// Contacts Detail for Items
+        .state('contacts.detail.item', {
+          // parent: 'contacts.detail',
+          url: '/item/:itemId',
+          views: {
+            '': {
+              templateUrl: 'contacts.detail.item.html',
+              controller:
+                [        '$scope', '$stateParams', '$state',
+                function ($scope,   $stateParams,   $state) {
+                  $scope.item = findById($scope.contact.items, $stateParams.itemId);
+                  $scope.edit = function () {
+                    $state.transitionTo('contacts.detail.item.edit', $stateParams);
+                  };
+                }],
+            },
+          },
+        })
+
+		// Contacts Detail for Items Editing
+        .state('contacts.detail.item.edit', {
+          views: {
+            '@contacts.detail': {
+              templateUrl: 'contacts.detail.item.edit.html',
+              controller:
+                [        '$scope', '$stateParams', '$state',
+                function ($scope,   $stateParams,   $state) {
+                  $scope.item = findById($scope.contact.items, $stateParams.itemId);
+                  $scope.done = function () {
+                    $state.transitionTo('contacts.detail.item', $stateParams);
+                  };
+                }],
+            },
+          },
+        })
+
+
+		/**
+		 * Homepage/Feed
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('home', {
+			url: '/',
+		})
+
+		/**
+		 * Stash Section
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('stash', {
+			url: '/stash',
+			templateUrl: 'stash.html',
+		})
+
+		/**
+		 * Add Section
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('add', {
+			url: '/add',
+			templateUrl: 'add.html',
+		})
+
+		/**
+		 * Search Section
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('search', {
+			url: '/search',
+			templateUrl: 'search.html',
+		})
+
+		/**
+		 * More Section
+		 *
+		 * @access        public
+		 * @author        Charles Johnson <me@charlesjohnson.org>
+		 * @copyright     Copyright (c) 2013, Charles Johnson
+		 */
+		.state('more', {
+			url: '/more',
+			templateUrl: 'more.html',
+		})
+
+
+	//END STATE PROVIDER ($stateProvider)
+    }])
+
+
+	// EXECUTE ANGULAR MODULE
+	.run(
+	[        '$rootScope', '$state', '$stateParams',
+	function ($rootScope,   $state,   $stateParams) {
+		$rootScope.$state = $state;
+		$rootScope.$stateParams = $stateParams;
+	}]);
+
+
+
+function findById(a, id) {
+	for (var i=0; i<a.length; i++) {
+		if (a[i].id == id) return a[i];
+	}
+}
+
+
+
+
---
+++ application/controllers/api.php
@@ -0,0 +1,112 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('api/index');
+	}
+
+
+	public function friends()
+	{
+
+
+		// This is sample data for friends
+		$data = array(
+			array(
+				'id' => 1,
+				'name' => "Charles Johnson",
+				'items' => array(
+					array(
+						'id' => 'a',
+						'type' => 'phone number',
+						'value' => '555-1234-1234',
+					),
+					array(
+						'id' => 'b',
+						'type' =>'email',
+						'value' => 'me@charlesjohnson.org',
+					)
+				),
+			),
+			array(
+				'id' => 33,
+				'name' => "Seal Goldman",
+				'items' => array(
+					array(
+						'id' => 'a',
+						'type' => 'blog',
+						'value' => 'http://seal.blogger.com',
+					),
+					array(
+						'id' => 'b',
+						'type' =>'fax',
+						'value' => '555-999-9999',
+					)
+				),
+			),
+			array(
+				'id' => 42,
+				'name' => "Aaron Smith",
+				'items' => array(
+					array(
+						'id' => 'a',
+						'type' => 'phone number',
+						'value' => '586-222-1234',
+					),
+					array(
+						'id' => 'b',
+						'type' =>'email',
+						'value' => 'aaront@gmail.com',
+					)
+				),
+			),
+			array(
+				'id' => 123,
+				'name' => "James Jackson",
+				'items' => array(
+					array(
+						'id' => 'a',
+						'type' => 'phone number',
+						'value' => '313-333-1234',
+					),
+					array(
+						'id' => 'b',
+						'type' =>'email',
+						'value' => 'jj@gmail.com',
+					)
+				)
+			)
+		);
+
+
+
+			
+		$this->output->set_output( json_encode( array( 'response' => TRUE, 'success' => TRUE, 'data' => $data ) ) );
+	}
+
+
+
+
+
+
+}
+
+
+
